9fe7460a5dafd7a40647060e32c84305a498c859
Yusuf <ynsereko@comcast.net>
2017-04-28 21:58:26 -0500

    Revert "4/26/17 commits"
    This reverts commit 41d74e9cecd36ef979a6676a84df7175400bcb40.

---
+++ IdentityServerFour/IdentityServerFour/Configuration/Clients.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerFour.Configuration
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client> {
+            new Client {
+                ClientId = "oauthClient",
+                ClientName = "Example Client Credentials Client Application",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = new List<Secret> {
+                    new Secret("superSecretPassword".Sha256())},
+                AllowedScopes = new List<string> {"customAPI.read"}
+            }
+        };
+        }
+    }
+}
